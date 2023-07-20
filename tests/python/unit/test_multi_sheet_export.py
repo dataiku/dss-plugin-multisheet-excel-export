66cac4ac94dc4b4bf25db706c9896d5d0cf217b8
vicperroux <vp2487@columbia.edu>
2023-07-20 14:01:52 -0700

    Update

--- tests/python/unit/test_multi_sheet_export.py
+++ tests/python/unit/test_multi_sheet_export.py
@@ -1,9 +1,10 @@
-from xlsx_writer import dataframes_to_xlsx
-
 import os
 import pandas as pd
 import tempfile
 
+#import sys
+#sys.path.insert(0, '/Users/victorperroux/Documents/Codes/GitHub/Plugin_Multisheet/dss-plugin-multisheet-excel-export')
+from python_lib.xlsx_writer import dataframes_to_xlsx
 
 def test_datasets_to_xlsx():
 
@@ -26,3 +27,12 @@ def test_datasets_to_xlsx():
 
     assert len(tables["df1"]) == len(df)
 
+if __name__ == "__main__":
+    df1 = pd.DataFrame({'dfId': [1], 'gender': ['M'], 'birthdate': ['1953/10/5']})
+    df2 = pd.DataFrame({'dfId': [2], 'gender': ['M']})
+    df3 = pd.DataFrame({'dfId': [3]})
+    tables = {'df1': df1, 'df2': df2, 'df3': df3}
+    def dataframe_provider(name):
+        return tables[name]
+    output_file ='/Users/victorperroux/Documents/Codes/GitHub/Plugin_Multisheet/dss-plugin-multisheet-excel-export/sample.xlsx'
+    dataframes_to_xlsx(['df1', 'df2', 'df3'], output_file, dataframe_provider)
