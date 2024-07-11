54830655cf1a3b060edaed4e44411fe30ba1eb52
Louis Barjon <louis.barjon@dataiku.com>
2024-07-11 11:16:26 +0200

    run formatting

--- custom-recipes/to-excel/recipe.py
+++ custom-recipes/to-excel/recipe.py
@@ -17,32 +17,31 @@ from dataiku.customrecipe import get_recipe_config
 from xlsx_writer import dataframes_to_xlsx
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='Multi-Sheet Excel Exporter | %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="Multi-Sheet Excel Exporter | %(levelname)s - %(message)s")
 
-input_datasets_ids = get_input_names_for_role('input_dataset')
+input_datasets_ids = get_input_names_for_role("input_dataset")
 if len(input_datasets_ids) == 0:
     logger.warning("Received no input datasets ids. input_datasets_ids={}".format(input_datasets_ids))
 
-input_datasets_names = [name.split('.')[-1] for name in input_datasets_ids]
+input_datasets_names = [name.split(".")[-1] for name in input_datasets_ids]
 if len(input_datasets_names) == 0:
-    logger.warning("Received Louis COMMIT no input datasets names. input_datasets_ids={}, input_datasets_names={}".format(
-        input_datasets_ids, input_datasets_names))
+    logger.warning("Received Louis COMMIT no input datasets names. input_datasets_ids={}, input_datasets_names={}".format(input_datasets_ids, input_datasets_names))
 
 # Retrieve the list of output folders, should contain unique element
-output_folder_id = get_output_names_for_role('folder')
+output_folder_id = get_output_names_for_role("folder")
 logger.info("Retrieved the following folder ids: {}".format(output_folder_id))
 output_folder_name = output_folder_id[0]
 logger.info("Received the following output folder name: {}".format(output_folder_name))
 output_folder = dataiku.Folder(output_folder_name)
 
 input_config = get_recipe_config()
-workbook_name = input_config.get('output_workbook_name', None)
+workbook_name = input_config.get("output_workbook_name", None)
 
 if workbook_name is None:
     logger.warning("Received input received recipe config: {}".format(input_config))
-    raise ValueError('Could not read the workbook name.')
+    raise ValueError("Could not read the workbook name.")
 
-output_file_name = '{}.xlsx'.format(workbook_name)
+output_file_name = "{}.xlsx".format(workbook_name)
 
 try:
     validate_filename(output_file_name)
@@ -56,7 +55,7 @@ with tempfile.NamedTemporaryFile() as tmp_file:
 
     dataframes_to_xlsx(input_datasets_names, tmp_file_path, lambda name: dataiku.Dataset(name).get_dataframe())
 
-    with open(tmp_file_path, 'rb', encoding=None) as f:
+    with open(tmp_file_path, "rb", encoding=None) as f:
         output_folder.upload_stream(output_file_name, f)
 
 
