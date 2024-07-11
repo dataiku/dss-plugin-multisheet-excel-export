5209c127ca9413544575d0f1b41a470282807eee
Louis Barjon <louis.barjon@dataiku.com>
2024-07-11 11:07:51 +0200

    Run formatting black 200 char

--- python-lib/xlsx_writer.py
+++ python-lib/xlsx_writer.py
@@ -19,19 +19,14 @@ from openpyxl.worksheet.worksheet import Worksheet
 import pandas as pd
 
 DATAIKU_TEAL = "FF2AB1AC"
-LETTER_WIDTH = 1.20 # Approximative letter width to scale column width
-MAX_LENGTH_TO_SHOW = 45 # Limit copied from DSS native excel exporter
+LETTER_WIDTH = 1.20  # Approximative letter width to scale column width
+MAX_LENGTH_TO_SHOW = 45  # Limit copied from DSS native excel exporter
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO, format='Multi-Sheet Excel Exporter | %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format="Multi-Sheet Excel Exporter | %(levelname)s - %(message)s")
 
-def style_header(worksheet: Worksheet, 
-                  font_name: str = "Calibri", 
-                  font_size: int = 11, 
-                  font_color : str = WHITE, 
-                  background_color : str = DATAIKU_TEAL,
-                  bold : bool = True
-                 ):
+
+def style_header(worksheet: Worksheet, font_name: str = "Calibri", font_size: int = 11, font_color: str = WHITE, background_color: str = DATAIKU_TEAL, bold: bool = True):
     """
     Style header of the worksheet
     """
@@ -46,13 +41,14 @@ def style_header(worksheet: Worksheet,
     no_border_side = Side(border_style=None)
     border = Border(left=no_border_side, right=no_border_side, top=no_border_side, bottom=no_border_side)
 
-    alignment = Alignment(vertical='bottom', horizontal='center')
+    alignment = Alignment(vertical="bottom", horizontal="center")
 
     for header_cell in worksheet[1]:
         header_cell.font = font
         header_cell.fill = fill
         header_cell.border = border
         header_cell.alignment = alignment
+
 
 def get_column_width(column: Tuple):
     """
@@ -73,15 +69,16 @@ def get_column_width(column: Tuple):
     # Computations from ExcelOutputFormatter.java ExcelOutputFormatter.footer
     average_length_cell = math.ceil(sum_length_cells / (len(column) + 1))
     max_length_cells = min(max_length_cells, MAX_LENGTH_TO_SHOW)
-    
-    if max_length_cells > 2 * average_length_cell: # if max length much bigger than average
+
+    if max_length_cells > 2 * average_length_cell:  # if max length much bigger than average
         length_to_show = int((max_length_cells + average_length_cell) / 2)
     else:
         length_to_show = max_length_cells
 
-    length_to_show = max(length_to_show, length_header) 
+    length_to_show = max(length_to_show, length_header)
 
     return length_to_show * LETTER_WIDTH
+
 
 def auto_size_column_width(worksheet: Worksheet):
     """
@@ -91,16 +88,13 @@ def auto_size_column_width(worksheet: Worksheet):
         logger.warn(f"No header row for worksheet {worksheet}. Column auto-size skipped.")
         return
 
-    dimension_holder = DimensionHolder(worksheet=worksheet) 
+    dimension_holder = DimensionHolder(worksheet=worksheet)
 
     column_indexes = range(worksheet.min_column, worksheet.max_column + 1)
     for index_column, column in zip(column_indexes, worksheet.iter_cols()):
 
         column_width = get_column_width(column)
-        dimension_holder[get_column_letter(index_column)] = ColumnDimension(worksheet, 
-                                                           min=index_column, 
-                                                           max=index_column,
-                                                           width=column_width)
+        dimension_holder[get_column_letter(index_column)] = ColumnDimension(worksheet, min=index_column, max=index_column, width=column_width)
     worksheet.column_dimensions = dimension_holder
 
 
@@ -112,13 +106,13 @@ def dataframes_to_xlsx(input_dataframes_names, xlsx_abs_path, dataframe_provider
     :return:
     """
     logger.info("Writing output xlsx file ...")
-    writer = pd.ExcelWriter(xlsx_abs_path, engine='openpyxl')
+    writer = pd.ExcelWriter(xlsx_abs_path, engine="openpyxl")
 
     for name in input_dataframes_names:
         df = dataframe_provider(name)
 
         logger.info("Writing dataset into excel sheet...")
-        df.to_excel(writer, sheet_name=name, index=False, encoding='utf-8')
+        df.to_excel(writer, sheet_name=name, index=False, encoding="utf-8")
 
         worksheet = writer.sheets.get(name)
 
